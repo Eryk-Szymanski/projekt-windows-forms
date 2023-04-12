9cb63b35456831635e8d23e36cc65dee8acaae4b
Eryk-Szymanski <e.szymanski.33@interia.pl>
2023-04-12 19:19:17 +0200

    Klasa warstwy, narzędzia użytkownika i dodawanie text boxów

--- Projekt/Form1.Designer.cs
+++ Projekt/Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Tło");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Strona 1", new System.Windows.Forms.TreeNode[] {
-            treeNode1});
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -63,27 +60,20 @@
             this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
+            this.brushButton = new System.Windows.Forms.ToolStripButton();
+            this.pencilButton = new System.Windows.Forms.ToolStripButton();
+            this.textBoxerButton = new System.Windows.Forms.ToolStripButton();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
-            this.treeView2 = new System.Windows.Forms.TreeView();
             this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.toolStrip2 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton10 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton11 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton12 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton13 = new System.Windows.Forms.ToolStripButton();
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -335,101 +325,44 @@
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3,
-            this.toolStripButton4,
-            this.toolStripButton5,
-            this.toolStripButton6,
-            this.toolStripButton7,
-            this.toolStripButton8,
-            this.toolStripButton9});
+            this.brushButton,
+            this.pencilButton,
+            this.textBoxerButton});
             this.toolStrip1.Location = new System.Drawing.Point(0, 28);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(30, 567);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripButton1
+            // brushButton
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.brushButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.brushButton.Image = ((System.Drawing.Image)(resources.GetObject("brushButton.Image")));
+            this.brushButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.brushButton.Name = "brushButton";
+            this.brushButton.Size = new System.Drawing.Size(27, 24);
+            this.brushButton.Text = "toolStripButton1";
+            this.brushButton.Click += new System.EventHandler(this.selectTool);
             // 
-            // toolStripButton2
+            // pencilButton
             // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.pencilButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.pencilButton.Image = ((System.Drawing.Image)(resources.GetObject("pencilButton.Image")));
+            this.pencilButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pencilButton.Name = "pencilButton";
+            this.pencilButton.Size = new System.Drawing.Size(27, 24);
+            this.pencilButton.Text = "toolStripButton2";
+            this.pencilButton.Click += new System.EventHandler(this.selectTool);
             // 
-            // toolStripButton3
+            // textBoxerButton
             // 
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton3.Text = "toolStripButton3";
-            // 
-            // toolStripButton4
-            // 
-            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
-            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton4.Text = "toolStripButton4";
-            // 
-            // toolStripButton5
-            // 
-            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
-            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton5.Text = "toolStripButton5";
-            // 
-            // toolStripButton6
-            // 
-            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
-            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton6.Name = "toolStripButton6";
-            this.toolStripButton6.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton6.Text = "toolStripButton6";
-            // 
-            // toolStripButton7
-            // 
-            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
-            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton7.Name = "toolStripButton7";
-            this.toolStripButton7.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton7.Text = "toolStripButton7";
-            // 
-            // toolStripButton8
-            // 
-            this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
-            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton8.Name = "toolStripButton8";
-            this.toolStripButton8.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton8.Text = "toolStripButton8";
-            // 
-            // toolStripButton9
-            // 
-            this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
-            this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton9.Name = "toolStripButton9";
-            this.toolStripButton9.Size = new System.Drawing.Size(27, 24);
-            this.toolStripButton9.Text = "toolStripButton9";
+            this.textBoxerButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.textBoxerButton.Image = ((System.Drawing.Image)(resources.GetObject("textBoxerButton.Image")));
+            this.textBoxerButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.textBoxerButton.Name = "textBoxerButton";
+            this.textBoxerButton.Size = new System.Drawing.Size(27, 24);
+            this.textBoxerButton.Text = "toolStripButton3";
+            this.textBoxerButton.Click += new System.EventHandler(this.selectTool);
             // 
             // splitContainer1
             // 
@@ -445,12 +378,11 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.treeView2);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
             this.splitContainer1.Panel2.Controls.Add(this.toolStrip2);
-            this.splitContainer1.Panel2.Controls.Add(this.treeView1);
             this.splitContainer1.Size = new System.Drawing.Size(897, 567);
-            this.splitContainer1.SplitterDistance = 687;
+            this.splitContainer1.SplitterDistance = 686;
             this.splitContainer1.TabIndex = 2;
             // 
             // pictureBox1
@@ -459,7 +391,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(668, 548);
+            this.pictureBox1.Size = new System.Drawing.Size(667, 548);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
@@ -469,7 +401,7 @@
             // vScrollBar1
             // 
             this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.vScrollBar1.Location = new System.Drawing.Point(668, 0);
+            this.vScrollBar1.Location = new System.Drawing.Point(667, 0);
             this.vScrollBar1.Name = "vScrollBar1";
             this.vScrollBar1.Size = new System.Drawing.Size(19, 548);
             this.vScrollBar1.TabIndex = 2;
@@ -479,22 +411,8 @@
             this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.hScrollBar1.Location = new System.Drawing.Point(0, 548);
             this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(687, 19);
+            this.hScrollBar1.Size = new System.Drawing.Size(686, 19);
             this.hScrollBar1.TabIndex = 0;
-            // 
-            // treeView2
-            // 
-            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView2.Location = new System.Drawing.Point(0, 20);
-            this.treeView2.Name = "treeView2";
-            treeNode1.Name = "Node1";
-            treeNode1.Text = "Tło";
-            treeNode2.Name = "Node0";
-            treeNode2.Text = "Strona 1";
-            this.treeView2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode2});
-            this.treeView2.Size = new System.Drawing.Size(206, 520);
-            this.treeView2.TabIndex = 3;
             // 
             // label1
             // 
@@ -502,9 +420,17 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.Size = new System.Drawing.Size(64, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Obiekty";
+            this.label1.Text = "Warstwy";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(206, 537);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // toolStrip2
             // 
@@ -517,7 +443,7 @@
             this.toolStripButton13});
             this.toolStrip2.Location = new System.Drawing.Point(0, 540);
             this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(206, 27);
+            this.toolStrip2.Size = new System.Drawing.Size(207, 27);
             this.toolStrip2.TabIndex = 1;
             this.toolStrip2.Text = "toolStrip2";
             // 
@@ -557,18 +483,8 @@
             this.toolStripButton13.Size = new System.Drawing.Size(29, 24);
             this.toolStripButton13.Text = "toolStripButton13";
             // 
-            // treeView1
-            // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(206, 567);
-            this.treeView1.TabIndex = 0;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 595);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip1);
@@ -626,17 +542,10 @@
         private ToolStripSeparator toolStripSeparator5;
         private ToolStripMenuItem aboutToolStripMenuItem;
         private ToolStrip toolStrip1;
-        private ToolStripButton toolStripButton1;
-        private ToolStripButton toolStripButton2;
-        private ToolStripButton toolStripButton3;
-        private ToolStripButton toolStripButton4;
-        private ToolStripButton toolStripButton5;
-        private ToolStripButton toolStripButton6;
-        private ToolStripButton toolStripButton7;
-        private ToolStripButton toolStripButton8;
-        private ToolStripButton toolStripButton9;
+        private ToolStripButton brushButton;
+        private ToolStripButton pencilButton;
+        private ToolStripButton textBoxerButton;
         private SplitContainer splitContainer1;
-        private TreeView treeView1;
         private VScrollBar vScrollBar1;
         private HScrollBar hScrollBar1;
         private Label label1;
@@ -645,7 +554,7 @@
         private ToolStripButton toolStripButton11;
         private ToolStripButton toolStripButton12;
         private ToolStripButton toolStripButton13;
-        private TreeView treeView2;
         private PictureBox pictureBox1;
+        private ListView listView1;
     }
 }
--- Projekt/Form1.cs
+++ Projekt/Form1.cs
@@ -9,39 +9,102 @@ namespace Projekt
         int y = -1;
         bool moving = false;
         Pen pen;
+        tools currentTool { get; set; }
+        List<Layer> layers { get; set; }
+        Layer currentLayer { get; set; }
+
+        // Narzêdzia z lewego tool boxa
+        public enum tools {
+            brush,
+            pencil,
+            textBoxer
+        }
         public Form1()
         {
             InitializeComponent();
-            g = pictureBox1.CreateGraphics();
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Ustawienie wartoœci pocz¹tkowych
+            currentTool = tools.brush;
+            layers = new List<Layer>();
+            currentLayer = new Layer("Warstwa 1", pictureBox1);
+            layers.Add(currentLayer);
+
+            // Wartoœci dla rysowania
             pen = new Pen(Color.Black, 5);
             pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            moving = true;
-            x = e.X;
-            y = e.Y;
-            pictureBox1.Cursor = Cursors.Cross;
+            if (currentTool == tools.brush || currentTool == tools.pencil)
+            {
+                moving = true;
+                x = e.X;
+                y = e.Y;
+                pictureBox1.Cursor = Cursors.Cross;
+            } else
+            {
+                TextBox newTextBox = new TextBox();
+                newTextBox.Location = e.Location;
+                newTextBox.Multiline = true;
+                newTextBox.BackColor = Color.White;
+                pictureBox1.Controls.Add(newTextBox);
+                currentLayer.textBoxes.Add(newTextBox);
+            }
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(moving && x != -1 && y != -1)
+            if (currentTool == tools.brush || currentTool == tools.pencil)
             {
-                g.DrawLine(pen, new Point(x, y), e.Location);
-                x = e.X;
-                y = e.Y;
+                if (moving && x != -1 && y != -1)
+                {
+                    currentLayer.graphics.DrawLine(pen, new Point(x, y), e.Location);
+                    x = e.X;
+                    y = e.Y;
+                }
             }
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            moving = false;
-            x = -1;
-            y = -1;
-            pictureBox1.Cursor = Cursors.Default;
+            if (currentTool == tools.brush || currentTool == tools.pencil)
+            {
+                moving = false;
+                x = -1;
+                y = -1;
+                pictureBox1.Cursor = Cursors.Default;
+            }
+        }
+
+        private void selectTool(object sender, EventArgs e)
+        {
+            ToolStripButton button = sender as ToolStripButton;
+            switch(button.Name)
+            {
+                case "brushButton":
+                    currentTool = tools.brush;
+                    break;
+                case "pencilButton":
+                    currentTool = tools.pencil;
+                    break;
+                case "textBoxerButton":
+                    currentTool = tools.textBoxer;
+                    break;
+            }
+        }
+    }
+    public class Layer
+    {
+        public List<TextBox> textBoxes { get; set; }
+        public Graphics graphics { get; set; }
+        public string Name { get; set; }
+        public Layer(string name, PictureBox pictureBox)
+        {
+            this.Name = name;
+            this.textBoxes = new List<TextBox>();
+            graphics = pictureBox.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
         }
     }
 }
